6bacf3ffe293334221352a0b37ec522b64538b80
Maksim Chalyh <nazgulius@yandex.ru>
2024-11-28 23:50:16 +0300

    update config

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,3 +1,5 @@
+import GamePlay from './GamePlay.js';
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
@@ -7,6 +9,13 @@ export default class GameController {
   init() {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
+    const img = document.getElementById('game-container');
+    //img.addEventListener('load', () => {
+    document.addEventListener('DOMContentLoaded', () => {
+      console.log('work');
+      gamePlay.drawUi('prairie');
+    })
+
   }
 
   onCellClick(index) {
